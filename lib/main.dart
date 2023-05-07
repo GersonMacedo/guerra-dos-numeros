24ec211a2cd8e38a6884ec6955514bffb899dff6
GersonMacedo <grsnvinicius24@gmail.com>
2023-05-07 15:52:28 -0300

    Add page routes and add Fittedbox to fix screen scale

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,71 +15,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int state = 0;
-
-  void changeState(int value) {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      state = value;
-    });
-  }
+class MainPage extends StatelessWidget  {
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      //Add a tranform matrix4 to scale to the correct screen size
       color: const Color(0xFF54436B),
-      padding: const EdgeInsets.all(19),
-      child: Column(
-        children: [
-          const Text(
-            "Guerra dos números, Robôs e coisas matemáticas",
-            style: const TextStyle(fontSize: 22, color: Colors.white)
-          ),
-          const SizedBox(height: 19),
-          Image.asset('assets/images/main.png'),
-          const SizedBox(height: 19),
-          SizedBox(
-            width: 150,
-            child: ElevatedButton(
-                onPressed: (){},
+      padding: const EdgeInsets.all(30),
+      child: FittedBox(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            const Text(
+              "Guerra dos números, Robôs e coisas matemáticas",
+              style: const TextStyle(fontSize: 22, color: Colors.white)
+            ),
+            const SizedBox(height: 15),
+            Image.asset('assets/images/main.png'),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const PlayPage()));
+                },
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
                 child: Row(
                   children: const [
@@ -85,13 +54,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text(" Jogar")
                   ],
                 )
+              ),
             ),
-          ),
-          const SizedBox(height: 19),
-          SizedBox(
-            width: 150,
-            child: ElevatedButton(
-                onPressed: (){},
+            const SizedBox(height: 19),
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const AchievementsPage()));
+                },
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
                 child: Row(
                   children: const [
@@ -99,13 +70,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text(" Conquistas")
                   ],
                 )
+              ),
             ),
-          ),
-          const SizedBox(height: 19),
-          SizedBox(
-            width: 150,
-            child: ElevatedButton(
-                onPressed: (){},
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
+                },
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
                 child: Row(
                   children: const [
@@ -113,13 +86,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text(" Configurações")
                   ],
                 )
+              ),
             ),
-          ),
-          const SizedBox(height: 19),
-          SizedBox(
-            width: 150,
-            child: ElevatedButton(
-                onPressed: (){},
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AboutPage()));
+                },
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
                 child: Row(
                   children: const [
@@ -127,10 +102,152 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text(" Sobre")
                   ],
                 )
-            ),
-          ),
-        ],
-      ),
+              ),
+            )
+          ],
+        )
+      )
     );
   }
 }
+
+class PlayPage extends StatelessWidget {
+  const PlayPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: const Color(0xFF54436B),
+        padding: const EdgeInsets.all(30),
+        child: FittedBox(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 15),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder()
+                    ),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: Text("Voltar", style: const TextStyle(fontSize: 22, color: Colors.black))
+                  )
+                ]
+              ),
+              const Text("TODO: tela Jogar")
+            ],
+          )
+        )
+    );
+  }
+}
+
+class AchievementsPage extends StatelessWidget {
+  const AchievementsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: const Color(0xFF54436B),
+        padding: const EdgeInsets.all(30),
+        child: FittedBox(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                Column(
+                    children: [
+                      const SizedBox(height: 15),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder()
+                          ),
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                          child: Text("Voltar", style: const TextStyle(fontSize: 22, color: Colors.black))
+                      )
+                    ]
+                ),
+                const Text("TODO: tela de conquistas")
+              ],
+            )
+        )
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: const Color(0xFF54436B),
+        padding: const EdgeInsets.all(30),
+        child: FittedBox(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                Column(
+                    children: [
+                      const SizedBox(height: 15),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder()
+                          ),
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                          child: Text("Voltar", style: const TextStyle(fontSize: 22, color: Colors.black))
+                      )
+                    ]
+                ),
+                const Text("TODO: tela de configurações")
+              ],
+            )
+        )
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: const Color(0xFF54436B),
+        padding: const EdgeInsets.all(30),
+        child: FittedBox(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                Column(
+                    children: [
+                      const SizedBox(height: 15),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder()
+                          ),
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                          child: Text("Voltar", style: const TextStyle(fontSize: 22, color: Colors.black))
+                      )
+                    ]
+                ),
+                const Text("TODO: tela de sobre")
+              ],
+            )
+        )
+    );
+  }
+}
+
