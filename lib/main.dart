e67f1c5ccd953b9547041856bc59da6350c4caf3
Richard <richard.esclima@gmail.com>
2023-07-18 23:23:23 -0300

    feat: adiciona listagem de itens em Ranking

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Home()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Home());
   }
 }
 
@@ -38,8 +37,8 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>{
-  _HomeState(){
+class _HomeState extends State<Home> {
+  _HomeState() {
     selected.add(Menu(changePage, key: UniqueKey()));
     bottomButtons.add(true);
     backButton.add(false);
@@ -50,14 +49,15 @@ class _HomeState extends State<Home>{
   List<bool> bottomButtons = [];
   List<bool> backButton = [];
 
-  void changePage(Widget? page, {bool bottom = true, bool back = true, bool keep = false}){
+  void changePage(Widget? page,
+      {bool bottom = true, bool back = true, bool keep = false}) {
     setState(() {
-      if(page == null){
+      if (page == null) {
         selected.removeLast();
         bottomButtons.removeLast();
         backButton.removeLast();
-      }else{
-        if(!keep){
+      } else {
+        if (!keep) {
           selected = [selected[0]];
           bottomButtons = [bottomButtons[0]];
           backButton = [backButton[0]];
@@ -77,48 +77,48 @@ class _HomeState extends State<Home>{
     double width = vertical ? 540 : 960;
     double height = vertical ? 960 : 540;
 
-    if(MediaQuery.of(context).size.width / width < MediaQuery.of(context).size.height / height){
-      height = MediaQuery.of(context).size.height * width / MediaQuery.of(context).size.width;
-    }else{
-      width = MediaQuery.of(context).size.width * height / MediaQuery.of(context).size.height;
+    if (MediaQuery.of(context).size.width / width <
+        MediaQuery.of(context).size.height / height) {
+      height = MediaQuery.of(context).size.height *
+          width /
+          MediaQuery.of(context).size.width;
+    } else {
+      width = MediaQuery.of(context).size.width *
+          height /
+          MediaQuery.of(context).size.height;
     }
 
     return WillPopScope(
-      onWillPop: (){
-        if(selected.last.key == const ValueKey("Menu")){
-          return Future(() => true);
-        }
+        onWillPop: () {
+          if (selected.last.key == const ValueKey("Menu")) {
+            return Future(() => true);
+          }
 
-        changePage(null);
-        return Future(() => false);
-      },
-      child: Material(
-        child: Container(
-          color: Colors.black,
-          child: FittedBox(
-            alignment: Alignment.topCenter,
+          changePage(null);
+          return Future(() => false);
+        },
+        child: Material(
             child: Container(
-                width: width,
-                height: height,
-                color: const Color(0xFF54436B),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    buildBackButton(),
-                    Expanded(child: selected.last),
-                    buildBottomButtons()
-                  ],
-                )
-            )
-          )
-        )
-      )
-    );
+                color: Colors.black,
+                child: FittedBox(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                        width: width,
+                        height: height,
+                        color: const Color(0xFF54436B),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            buildBackButton(),
+                            Expanded(child: selected.last),
+                            buildBottomButtons()
+                          ],
+                        ))))));
   }
 
-  Widget buildBackButton(){
-    if(!backButton.last){
+  Widget buildBackButton() {
+    if (!backButton.last) {
       return Container();
     }
 
@@ -127,66 +127,68 @@ class _HomeState extends State<Home>{
         width: 100,
         padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
         child: ElevatedButton(
-            onPressed: (){changePage(null);},
+            onPressed: () {
+              changePage(null);
+            },
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 5),
-              shape: const RoundedRectangleBorder()
-            ),
+                backgroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                shape: const RoundedRectangleBorder()),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
-                Text("  Voltar  ", style: TextStyle(color: Colors.black, fontSize: 18))
-              ]
-            )
-        )
-    );
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
+                  Text("  Voltar  ",
+                      style: TextStyle(color: Colors.black, fontSize: 18))
+                ])));
   }
 
-  Widget buildBottomButtons(){
-    if(!bottomButtons.last){
+  Widget buildBottomButtons() {
+    if (!bottomButtons.last) {
       return Container();
     }
 
     return Container(
-      height: 50,
-      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
+        height: 50,
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           TextButton(
-            onPressed: (){changePage(Achievements());},
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/medal.png'),
-                const Text("Conquistas", style: TextStyle(color: Colors.white, fontSize: 22))
-              ],
-            )
-          ),
+              onPressed: () {
+                changePage(Achievements());
+              },
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/medal.png'),
+                  const Text("Conquistas",
+                      style: TextStyle(color: Colors.white, fontSize: 22))
+                ],
+              )),
           TextButton(
-              onPressed: (){changePage(About());},
+              onPressed: () {
+                changePage(About());
+              },
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: const [
                   Icon(Icons.info_outline, color: Colors.white, size: 30),
-                  Text(" Sobre", style: TextStyle(color: Colors.white, fontSize: 22))
+                  Text(" Sobre",
+                      style: TextStyle(color: Colors.white, fontSize: 22))
                 ],
-              )
-          ),
+              )),
           TextButton(
-              onPressed: (){changePage(Ranking());},
+              onPressed: () {
+                changePage(Ranking());
+              },
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: const [
-                  Icon(Icons.emoji_events_outlined, color: Colors.white, size: 30),
-                  Text(" Ranking", style: TextStyle(color: Colors.white, fontSize: 20))
+                  Icon(Icons.emoji_events_outlined,
+                      color: Colors.white, size: 30),
+                  Text(" Ranking",
+                      style: TextStyle(color: Colors.white, fontSize: 20))
                 ],
-              )
-          )
-        ]
-      )
-    );
+              ))
+        ]));
   }
 }
--- lib/pages/ranking.dart
+++ lib/pages/ranking.dart
@@ -1,21 +1,181 @@
 import 'package:flutter/material.dart';
 
-//TODO: toda a pagina de ranking
-
 class Ranking extends StatelessWidget {
-  const Ranking({super.key});
+  const Ranking({Key? key}) : super(key: key);
+
+  static const usersList = [
+    {
+      'name': 'Paulo José',
+      'score': 1000,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Paulo José',
+      'score': 1000,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Paulo José',
+      'score': 1000,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Paulo José',
+      'score': 1000,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+    {
+      'name': 'Ana Maria',
+      'score': 500,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            SizedBox(height: 15),
-            Text("TODO: tela de ranking")
-          ],
-        )
+      padding: const EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.width * 0.8,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 50),
+          Container(
+              margin: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Text(
+                'Ranking',
+                style: TextStyle(fontSize: 60, color: Colors.white),
+              )),
+          Expanded(
+            child: ListView(
+              children: usersList
+                  .map((user) => Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: _buildListItem(user),
+                      ))
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem(Map<String, dynamic> user) {
+    String name = user['name'];
+    int score = user['score'];
+
+    return Container(
+      height: 64,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(8.0),
+            height: 64,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Color(0xFF212A3E),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8.0),
+                bottomRight: Radius.circular(8.0),
+              ),
+            ),
+            child: Text(
+              '$score',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
